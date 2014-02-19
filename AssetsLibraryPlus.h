88bf2b8beda428339920a5dd9b75d82859cdaf80
Max <mczonk@gmail.com>
2014-02-19 14:19:27 +0100

    Fixed Umbrealla Header

--- AssetsLibraryPlus.h
+++ AssetsLibraryPlus.h
@@ -1,5 +1,5 @@
 #import <AssetsLibraryPlus/ALAssetRepresentation+ALPData.h>
 
-//#import <AssetsLibraryPlus/ALAssetsLibrary+ALPGroupWithAlbumName.h>
+#import <AssetsLibraryPlus/ALAssetsLibrary+ALPGroupWithAlbumName.h>
 #import <AssetsLibraryPlus/ALAssetsLibrary+ALPLastAsset.h>
-//#import <AssetsLibraryPlus/ALAssetsLibrary+ALPWriteToAssetsGroup.h>
+#import <AssetsLibraryPlus/ALAssetsLibrary+ALPWriteToAssetsGroup.h>
